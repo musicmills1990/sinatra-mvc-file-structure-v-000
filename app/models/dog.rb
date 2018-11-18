82a0581302ef5a55fcbf42c52c602476f66e32f6
Marc Mills <mdmills1990@gmail.com>
2018-11-18 19:07:36 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,11 +1,13 @@
 class Dog < Sinatra::Base
 
 attr_accessor :name, :breed, :age
+@@all = []
 
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
+    @@all << self
   end
 
 end
